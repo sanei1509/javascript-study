03646f13d25f5100f0a9fd5c5ba0c6f78090cb13
Santiago Neira <4373@holbertonschool.com>
2022-06-21 00:13:31 +0000

    viendo el scope de js

---
+++ P.O.O/scope.js
@@ -0,0 +1,11 @@
+//definamos una variable global
+const msg = "Hola a todos";
+
+function myFun() {
+	cons msg = "Hola";
+	return msg;
+	/*
+	devolvera "Hola" ya que la local es
+	es la variable actual.
+	*/
+}
