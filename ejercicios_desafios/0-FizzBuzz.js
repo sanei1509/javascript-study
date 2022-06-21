858f06b993d0feba20ea80fff21201281b0342a6
Santiago Neira <4373@holbertonschool.com>
2022-06-21 01:54:01 +0000

    fizz buzz challenge

---
+++ ejercicios_desafios/0-FizzBuzz.js
@@ -0,0 +1,22 @@
+//FIZZ BUZZ : 
+//en que consiste ? 
+//function that print NUMBERS 1 TO 100
+//NUMBER MULTIPLO DE 5 Y 3 -> print FIZZ BUZZ
+//MULTIPLO DE 5 -> PRINT FIZZ
+//MULTIPLO DE 3 -> PRINT BUZZ
+
+for (let i = 1; i <= 100; i++) {
+	const tres_mult = i % 3 == 0;
+	const cinco_mult = i % 5 == 0;
+
+	//debemos saber primero si se cumple mayor cant. casos
+	if (tres_mult && cinco_mul)
+		console.log("FizzBuzz");
+	if (tres_mult)
+		console.log("Fizz");
+	if (cinco_mult)
+		console.log("Buzz");
+	else
+		console.log(i);
+
+}
