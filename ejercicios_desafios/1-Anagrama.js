60af4807953c3090d0fd194bddd9b6acd6093bd5
Santiago Neira <4373@holbertonschool.com>
2022-06-23 02:23:19 +0000

    intentando resolver el probelema de anagrama

--- ejercicios_desafios/1-Anagrama.js
+++ ejercicios_desafios/1-Anagrama.js
@@ -5,3 +5,20 @@
 //-> no hace falta comprobar que ambas palabras exista
 //-> 2 palabras IGUALES no son anagrama
 
+function isAnagram(wordOne, wordTwo)
+{
+	if (wordOne.toLowerCase() == wordTwo.toLowerCase())
+		return False;
+
+	//1 - necesitamos convertir a un array de caracteres ambas cadenas
+	//2 - ordenar alfbeticamente el array y comparar si son iguales
+	const arr1 = wordOne.toLowerCase.split()
+	const arr2 = wordTwo.toLowerCase().split()
+	arr1.sort()
+	arr2.sort()
+	
+	if(arr1 == arr2)
+		return True;
+	
+}
+
