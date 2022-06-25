da0e2fe1c63bff730fffae026bfcec67b72d6444
Santiago <santiagoneira52@gmail.com>
2022-06-25 20:56:18 -0300

    añadiendo funcionalidad de clan aleatorio

--- vue_JS/curso-intr-blueweb/01.js
+++ vue_JS/curso-intr-blueweb/01.js
@@ -4,24 +4,31 @@ createApp({
     data() {
         return{
         mensaje: "Mensaje con vue",
+        clanes: ["Uzumaki", "Uchiha", "Hyuga", "Senju", "Otsutsuki"],
         personajes: [
             {nombre: "Naruto", clan: "Uzumaki"},
-            {nombre: "Sasuke", clan: "Uchiha"},
+            {nombre: "Sasuke", clan: ''},
             {nombre: "Itachi", clan: "Uchiha"}
         ],
         nuevo_personaje: '',
+        clan_name: ''
     }
     },
     methods:{
         crearPersonaje() {
             //no podemos acceder directamente a personajes porque esta dentro de data
-            if (this.nuevo_personaje != ''){
+            if (this.nuevo_personaje != '' && this.clan_name != ''){
             this.personajes.push(
-                {nombre: this.nuevo_personaje, clan: 0}
+                {nombre: this.nuevo_personaje, clan: this.clan_name}
                 )
             //pusheamos y limpiamos el input
             this.nuevo_personaje = ''
+            this.clan_name = ''
             }
+        },
+        random_clan() {
+            let clan_index = Math.round(Math.random()*4)
+            return this.clanes[clan_index]
         }
     }
 }).mount('#app') // lo vamos a montar en #app
