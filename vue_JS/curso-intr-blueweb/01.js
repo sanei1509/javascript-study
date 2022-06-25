0c6f88d60c47609a89a0c04c004d899a094b8697
Santiago <santiagoneira52@gmail.com>
2022-06-25 19:51:13 -0300

    método para crear personajes

--- vue_JS/curso-intr-blueweb/01.js
+++ vue_JS/curso-intr-blueweb/01.js
@@ -9,8 +9,20 @@ createApp({
             {nombre: "Sasuke", clan: "Uchiha"},
             {nombre: "Itachi", clan: "Uchiha"}
         ],
-        nuevos_personajes: ''
+        nuevo_personaje: '',
     }
+    },
+    methods:{
+        crearPersonaje() {
+            //no podemos acceder directamente a personajes porque esta dentro de data
+            if (this.nuevo_personaje != ''){
+            this.personajes.push(
+                {nombre: this.nuevo_personaje, clan: 0}
+                )
+            //pusheamos y limpiamos el input
+            this.nuevo_personaje = ''
+            }
+        }
     }
 }).mount('#app') // lo vamos a montar en #app
 
