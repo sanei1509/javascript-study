d540ce15cc19270ba0424fc5210293a198da5117
santiago <santiagoneira52@gmail.com>
2022-09-12 08:40:38 -0300

    do a challenge with fecth

--- async-await/primeras-pruebas/app.js
+++ async-await/primeras-pruebas/app.js
@@ -9,3 +9,22 @@ const extraerPost = (id) => {
 };
 
 extraerPost(6);
+
+// DESAFIO
+// ¿ Quién escribio el [post numero 80]
+const extraerUser = (id) => {
+  BODY = document.body;
+  //   fetch a una url específica
+  fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res, err) => {
+    post = res.json();
+    post.then((res) => {
+      alert(`esta por ver el post numero ${res.id}`);
+      fetch(`https://jsonplaceholder.typicode.com/users/${res.userId}`)
+        .then((res) => res.json())
+        .then((user) => alert(user.name));
+    });
+    if (err) alert(err);
+  });
+};
+
+extraerUser(80);
