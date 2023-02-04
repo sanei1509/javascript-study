2faadf50b5d0dee153299b1c5dd59aaf31203fbf
santiago <santiagoneira52@gmail.com>
2023-02-04 13:11:47 -0300

    aprendizaje continuo

--- P.O.O/00-practica-conceptos.js
+++ P.O.O/00-practica-conceptos.js
@@ -3,6 +3,7 @@
 const personaje = function (realidad, personaje) {
   this.reality = realidad;
   this.name = personaje;
+  this.c;
 };
 
 const naruto = new personaje("Naruto", "Naruto Uzumaki");
@@ -21,8 +22,20 @@ console.log(naruto);
 
 // Los objetos creados a partir de un prototipo comparten las mismas propiedades y metodos
 
-personaje.prototype.action = function () {
+personaje.prototype.actionTrigger = function () {
   console.log(`Clones de sombras`);
 };
 
 console.log(personaje.prototype);
+
+// creo una mesa en poo
+
+const Mesa = function (largo, ancho, agrandar) {
+  this.largo = largo;
+  this.ancho = ancho;
+  // Los métodos no debería ir mismo dentro de la función constructora
+  this.agrandar = ancho + 10;
+};
+
+// DEBEÍAMOS HACER USO DE LA HERENCIA PROTOTIPICA
+Mesa.prototype.agrandar = function (ancho) {};
